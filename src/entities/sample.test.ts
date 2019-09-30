90d1179f22d9f78812c2ff152038d0ddba0f3294
MacKenzie, Dylan <dylan.mackenzie@ncr.com>
2019-09-30 15:08:53 -0400

    adding .dockerignore, mocha and chai, sample tests

---
+++ src/entities/sample.test.ts
@@ -0,0 +1,10 @@
+import { describe, it } from 'mocha';
+
+export const sampleTests = () => {
+  describe("Sample test running", () => {
+    it('Should pass', (done) => {
+      done()
+    })
+  })
+}
+
